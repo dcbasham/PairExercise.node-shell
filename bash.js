2ff73e8ade314823af66adcef60421cd8f60ffdf
Fiona Murray <fiona.c.murray@gmail.com>
2022-06-15 16:55:50 -0400

    ls and pwd added

--- bash.js
+++ bash.js
@@ -1,6 +1,14 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+  process.stdout.write('You typed: ' + cmd + '\n');
+  if(cmd === 'pwd'){
+    pwd();
+  }
+  if(cmd === 'ls'){
+    ls();
+  }
+
 });
